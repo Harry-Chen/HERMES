1b1dc65ffd9250db09c837b6bbb081ad303b7358
Jiajie Chen <noc@jiegec.ac.cn>
2019-06-06 10:02:24 +0800

    Add zero file test

--- test.sh
+++ test.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
+rm test_file
+rm test_meta
+mkdir -p mount
+./HERMES --filedev=test_file --metadev=test_meta -f ./mount &
+ID=$!
+
 dd if=/dev/urandom of=test_random count=1000 2>/dev/null
 echo "Expected:"
 sha1sum test_random
-mkdir -p mount
-./HERMES --filedev=test_file --metadev=meta_dev -f ./mount &
-ID=$!
 cp test_random mount/
 echo "Actual:"
 sha1sum mount/test_random
+
+dd if=/dev/zero of=test_zero count=1000 2>/dev/null
+echo "Expected:"
+sha1sum test_zero
+cp test_zero mount/
+echo "Actual:"
+sha1sum mount/test_zero
+
 kill $ID
